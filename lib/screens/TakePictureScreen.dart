fe851e3eea308e84f2d3b770d713b11b876fa57f
Veer Nagda <veer742003@gmail.com>
2024-02-28 00:00:16 +0530

    Added overlay

--- lib/screens/TakePictureScreen.dart
+++ lib/screens/TakePictureScreen.dart
@@ -1,12 +1,12 @@
 import 'dart:io';
-
-import 'package:camera/camera.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_background_service/flutter_background_service.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:go_router/go_router.dart';
+import 'package:image/image.dart' as img;
+import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:ui/services/api_service.dart';
 
@@ -14,7 +14,7 @@ import '../services/shared_service.dart';
 
 // A screen that allows users to take a picture using a given camera.
 class TakePictureScreen extends StatefulWidget {
-  const TakePictureScreen({Key? key}) : super(key: key);
+  const TakePictureScreen({super.key});
 
   @override
   TakePictureScreenState createState() => TakePictureScreenState();
@@ -23,7 +23,8 @@ class TakePictureScreen extends StatefulWidget {
 class TakePictureScreenState extends State<TakePictureScreen> {
   late final CameraController _controller;
   late Future<void>? _initializeControllerFuture;
-
+  static late double xCrop;
+  static late double yCrop;
   @override
   void initState() {
     super.initState();
@@ -36,16 +37,53 @@ class TakePictureScreenState extends State<TakePictureScreen> {
     PermissionStatus status = await Permission.camera.status;
     if (!status.isGranted) {
       PermissionStatus permissionStatus = await Permission.camera.request();
-      PermissionStatus permissionLocationStatus =
-          await Permission.locationAlways.request();
-      LocationPermission permission;
-      permission = await Geolocator.requestPermission();
-      if (permissionStatus != PermissionStatus.granted &&
-          permissionLocationStatus != PermissionStatus.granted) {
+      if (permissionStatus != PermissionStatus.granted) {
         // Permission was denied
         // You may want to disable functionality that depends on the camera permission.
       }
     }
+  }
+
+
+  _cropImage(XFile image) async {
+    File imageFile = File(image.path);
+
+    // Read the image bytes
+    List<int> imageBytes = await imageFile.readAsBytes();
+
+    // Decode the image bytes
+    img.Image? capturedImage = img.decodeImage(Uint8List.fromList(imageBytes));
+
+    // Calculate crop dimensions (you need to define your own values)
+    double doubleLeft = xCrop; // Define your cropping values
+    double doubleTop = yCrop;
+    double doubleWidth = 210;
+    double doubleHeight = 210;
+
+    int newLeft = (doubleLeft * capturedImage!.width).toInt();
+    int newTop = (doubleTop * capturedImage.height).toInt();
+    int newWidth = (doubleWidth * capturedImage.width).toInt();
+    int newHeight = (doubleHeight * capturedImage.height).toInt();
+
+    // Crop the image
+    img.Image croppedImage = img.copyCrop(
+    capturedImage,
+    newLeft,
+    newTop,
+    newWidth,
+    newHeight,
+    );
+
+    // Save the cropped image to a temporary file
+    Directory tempDir = await getTemporaryDirectory();
+    String tempPath = tempDir.path;
+    String filePath = '$tempPath/cropped_image.jpg';
+
+    File croppedFile = File(filePath);
+    await croppedFile.writeAsBytes(img.encodeJpg(croppedImage));
+
+    // Convert the cropped file back to XFile
+    return XFile(filePath);
   }
 
   void initCamera() async {
@@ -69,6 +107,7 @@ class TakePictureScreenState extends State<TakePictureScreen> {
     super.dispose();
   }
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -80,22 +119,19 @@ class TakePictureScreenState extends State<TakePictureScreen> {
               _controller.value.isInitialized) {
             return Stack(
               children: [
-                CameraPreview(_controller),
                 Positioned(
-                  top: 100, // Adjust the Y position as needed
-                  left: 50, // Adjust the X position as needed
+                  left: 0,
+                  top: 0,
                   child: Container(
-                    width: 210.0, // Adjust the size of the square as needed
-                    height: 210.0,
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.white,
-                        width: 2.0,
-                      ),
-                      borderRadius: BorderRadius.circular(10.0),
-                      color: Colors.transparent,
-                    ),
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height,
+                    color: Colors.blue
+                        .withOpacity(0.5), // Adjust opacity and color as needed
                   ),
+                ),
+                ClipRect(
+                  clipper: MyClip(),
+                  child: CameraPreview(_controller),
                 ),
               ],
             );
@@ -108,11 +144,11 @@ class TakePictureScreenState extends State<TakePictureScreen> {
         onPressed: () async {
           try {
             await _initializeControllerFuture;
-            final image = await _controller.takePicture();
-
+            XFile rawImage = await _controller.takePicture();
+            final image = await _cropImage(rawImage);
             if (!mounted) return;
             int response = await APIService.doMultipartImagePost(
-                path: "/user/event/verify-user", image: image);
+                path: "/user/event/verify-user", image: rawImage);
             if (response == 200) {
               FlutterBackgroundService().startService();
               Map<String, dynamic> data = {
@@ -140,7 +176,7 @@ class TakePictureScreenState extends State<TakePictureScreen> {
   }
 }
 
-// A widget that displays the picture taken by the user.
+/*// A widget that displays the picture taken by the user.
 class DisplayPictureScreen extends StatelessWidget {
   final String imagePath;
 
@@ -152,5 +188,21 @@ class DisplayPictureScreen extends StatelessWidget {
       appBar: AppBar(title: const Text('Display the Picture')),
       body: Image.file(File(imagePath)),
     );
+  }
+}*/
+
+class MyClip extends CustomClipper<Rect> {
+  @override
+  Rect getClip(Size size) {
+    Random random = Random();
+    TakePictureScreenState.yCrop = 55 + random.nextDouble() * 255; // Adjust the Y position as needed
+    TakePictureScreenState.xCrop = 5 + random.nextDouble() * 195; // Adjust the X position as needed
+
+    return Rect.fromLTWH(TakePictureScreenState.xCrop, TakePictureScreenState.yCrop, 210, 210);
+  }
+
+  @override
+  bool shouldReclip(oldClipper) {
+    return false;
   }
 }
