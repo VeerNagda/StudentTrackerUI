7cfecd424f055808c418daae2f1e1fde0477a231
veer <veer742003@gmail.com>
2024-02-28 20:49:54 +0530

    bug fixes

--- lib/models/location/location_request_model.dart
+++ lib/models/location/location_request_model.dart
@@ -2,24 +2,47 @@ import 'package:latlong2/latlong.dart';
 
 class LocationRequestModel {
   late String sapId;
-  late DateTime dateTime;
   late String eventID;
-  late List<LatLng> coordinates;
+  late List<TimedCoordinates> timedCoordinates = [];
 
   LocationRequestModel({required this.sapId, required this.eventID});
 
   LocationRequestModel.fromJson(Map<String, dynamic> json) {
     sapId = json['SAP_ID'];
-    dateTime = json['DateTime'];
     eventID = json['Event_ID'];
+    if (json['timedCoordinates'] != null) {
+      timedCoordinates = [];
+      json['timedCoordinates'].forEach((v) {
+        timedCoordinates.add(TimedCoordinates.fromJson(v));
+      });
+    }
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
     data['SAP_ID'] = sapId;
-    data['DateTime'] = dateTime;
     data['Event_ID'] = eventID;
-    data['coordinates'] = coordinates.map((v) => v.toJson()).toList();
+    data['timedCoordinates'] =
+        timedCoordinates.map((v) => v.toJson()).toList();
+      return data;
+  }
+}
+
+class TimedCoordinates {
+  late List<LatLng> coordinates;
+  late String dateTime;
+
+  TimedCoordinates({required this.coordinates, required this.dateTime});
+
+  TimedCoordinates.fromJson(Map<String, dynamic> json) {
+    coordinates = json['coordinates'].cast<double>();
+    dateTime = json['DateTime'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['coordinates'] = coordinates;
+    data['DateTime'] = dateTime;
     return data;
   }
 }
