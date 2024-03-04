503e30593c0e2cf6ead38fec08edbc260e4d3d14
Veer Nagda <veer742003@gmail.com>
2024-03-05 00:22:42 +0530

    added crop image

--- lib/screens/TakePictureScreen.dart
+++ lib/screens/TakePictureScreen.dart
@@ -1,12 +1,14 @@
+import 'dart:io';
 import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_background_service/flutter_background_service.dart';
 import 'package:go_router/go_router.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:ui/services/api_service.dart';
-
+import 'package:image/image.dart' as img;
 
 // A screen that allows users to take a picture using a given camera.
 class TakePictureScreen extends StatefulWidget {
@@ -52,48 +54,6 @@ class TakePictureScreenState extends State<TakePictureScreen> {
       frontCamera,
       ResolutionPreset.medium,
     );
-    /*
-  _cropImage(XFile image) async {
-    File imageFile = File(image.path);
-
-    // Read the image bytes
-    List<int> imageBytes = await imageFile.readAsBytes();
-
-    // Decode the image bytes
-    img.Image? capturedImage = img.decodeImage(Uint8List.fromList(imageBytes));
-
-    // Calculate crop dimensions (you need to define your own values)
-    double doubleLeft = xCrop; // Define your cropping values
-    double doubleTop = yCrop;
-    double doubleWidth = 210;
-    double doubleHeight = 210;
-
-    int newLeft = (doubleLeft * capturedImage!.width).toInt();
-    int newTop = (doubleTop * capturedImage.height).toInt();
-    int newWidth = (doubleWidth * capturedImage.width).toInt();
-    int newHeight = (doubleHeight * capturedImage.height).toInt();
-
-    // Crop the image
-    img.Image croppedImage = img.copyCrop(
-      capturedImage,
-      newLeft,
-      newTop,
-      newWidth,
-      newHeight,
-    );
-
-    // Save the cropped image to a temporary file
-    Directory tempDir = await getTemporaryDirectory();
-    String tempPath = tempDir.path;
-    String filePath = '$tempPath/cropped_image.jpg';
-
-    File croppedFile = File(filePath);
-    await croppedFile.writeAsBytes(img.encodeJpg(croppedImage));
-
-    // Convert the cropped file back to XFile
-    return XFile(filePath);
-  }
-*/
 
     setState(() {});
     _initializeControllerFuture = _controller.initialize();
@@ -105,6 +65,31 @@ class TakePictureScreenState extends State<TakePictureScreen> {
     super.dispose();
   }
 
+  Future<XFile> cropImage(XFile imageFile) async {
+    // Load the image file
+    final data = await imageFile.readAsBytes();
+    final image = img.decodeImage(data);
+
+    // Calculate the cropping rectangle based on the clip area
+    final left = xCrop.toInt();
+    final top = yCrop.toInt();
+    const width = 240; // Width of the clip area
+    const height = 240; // Height of the clip area
+
+    // Create a temporary directory to save the cropped image
+    final tempDir = await getTemporaryDirectory();
+    final tempPath = tempDir.path;
+    final tempFile = File('$tempPath/cropped_image.jpg');
+
+    // Crop the image using the calculated rectangle and save it to the temporary file
+    final croppedImage = img.copyCrop(image!, left, top, width, height);
+    await tempFile
+        .writeAsBytes(Uint8List.fromList(img.encodeJpg(croppedImage)));
+
+    // Return the XFile of the cropped image
+    return XFile(tempFile.path);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -112,11 +97,22 @@ class TakePictureScreenState extends State<TakePictureScreen> {
       body: FutureBuilder<void>(
         future: _initializeControllerFuture,
         builder: (context, snapshot) {
+          if (!_controller.value.isInitialized) {
+            return const Card(child: Text('No Camera to Preview'));
+          }
+          var tmp = MediaQuery.of(context).size;
+          final screenH = max(tmp.height, tmp.width);
+          final screenW = min(tmp.height, tmp.width);
+          tmp = _controller.value.previewSize!;
+          final previewH = max(tmp.height, tmp.width);
+          final previewW = min(tmp.height, tmp.width);
+          final screenRatio = screenH / screenW;
+          final previewRatio = previewH / previewW;
           if (snapshot.connectionState == ConnectionState.done &&
               _controller.value.isInitialized) {
             return Stack(
               children: [
-                Positioned(
+                /*Positioned(
                   left: 0,
                   top: 0,
                   child: Container(
@@ -125,10 +121,23 @@ class TakePictureScreenState extends State<TakePictureScreen> {
                     color: Colors.blue
                         .withOpacity(0.5), // Adjust opacity and color as needed
                   ),
-                ),
+                ),*/
                 ClipRect(
                   clipper: MyClip(),
-                  child: CameraPreview(_controller),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(22),
+                    child: OverflowBox(
+                      maxHeight: screenRatio > previewRatio
+                          ? screenH
+                          : screenW / previewW * previewH,
+                      maxWidth: screenRatio > previewRatio
+                          ? screenH / previewH * previewW
+                          : screenW,
+                      child: CameraPreview(
+                        _controller,
+                      ),
+                    ),
+                  ),
                 ),
               ],
             );
@@ -142,14 +151,15 @@ class TakePictureScreenState extends State<TakePictureScreen> {
           try {
             await _initializeControllerFuture;
             XFile rawImage = await _controller.takePicture();
-            //final image = await _cropImage(rawImage);
+
+            XFile croppedImage = await cropImage(rawImage);
             if (!mounted) return;
             int response = await APIService.doMultipartImagePost(
-                path: "/user/event/verify-user", image: rawImage);
+                path: "/user/event/verify-user", image: croppedImage);
             if (response == 200) {
               /**/
               FlutterBackgroundService().startService();
-
+              setState(() {});
               if (kDebugMode) {
                 print("sent data");
               }
@@ -173,13 +183,29 @@ class MyClip extends CustomClipper<Rect> {
   @override
   Rect getClip(Size size) {
     Random random = Random();
+    /*maxHeight: screenRatio > previewRatio
+        ? screenH
+        : screenW / previewW * previewH;
+    maxWidth: screenRatio > previewRatio
+    ? screenH / previewH * previewW
+        : screenW;
+    double maxHeight= screenRatio > previewRatio
+        ? screenH
+        : screenW / previewW * previewH;
+    double maxWidth= screenRatio > previewRatio
+        ? screenH / previewH * previewW
+        : screenW;
     TakePictureScreenState.yCrop =
-        55 + random.nextDouble() * 255; // Adjust the Y position as needed
+        10 + random.nextDouble() * maxHeight; // Adjust the Y position as needed
     TakePictureScreenState.xCrop =
-        5 + random.nextDouble() * 195; // Adjust the X position as needed
+        5 + random.nextDouble() * maxWidth; // Adjust the X position as needed*/
+    TakePictureScreenState.yCrop =
+        55 + random.nextDouble() * 350; // Adjust the Y position as needed
+    TakePictureScreenState.xCrop =
+        5 + random.nextDouble() * 110; // Adjust the X position as needed
 
     return Rect.fromLTWH(
-        TakePictureScreenState.xCrop, TakePictureScreenState.yCrop, 210, 210);
+        TakePictureScreenState.xCrop, TakePictureScreenState.yCrop, 240, 240);
   }
 
   @override
