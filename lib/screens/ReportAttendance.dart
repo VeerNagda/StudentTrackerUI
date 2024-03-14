84d1f99b24cf058017edbe98919c579acda69573
Parinaz-Bharucha <parinazbharucha14@gmail.com>
2024-03-14 23:16:16 +0530

    download

--- lib/screens/ReportAttendance.dart
+++ lib/screens/ReportAttendance.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:excel/excel.dart';
 import 'package:go_router/go_router.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
 
 class ReportAttendance extends StatelessWidget {
-  final List<ReportAttendance>? events;
-
-  const ReportAttendance({super.key, this.events});
+  const ReportAttendance({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -59,7 +60,7 @@ class ReportAttendance extends StatelessWidget {
 }
 
 class DownloadButton extends StatelessWidget {
-  const DownloadButton({super.key});
+  const DownloadButton({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -81,7 +82,7 @@ class DownloadButton extends StatelessWidget {
           actions: <Widget>[
             TextButton(
               onPressed: () {
-                // TODO Perform download action here
+                _downloadFile(context);
                 context.pop();
               },
               child: const Text("Yes"),
@@ -95,6 +96,34 @@ class DownloadButton extends StatelessWidget {
           ],
         );
       },
+    );
+  }
+
+  void _downloadFile(BuildContext context) async {
+    final Excel excel = Excel.createExcel();
+    final Sheet sheet = excel['Sheet1'];
+
+    // Add sample data to the Excel sheet
+    sheet.appendRow([]);
+
+    // Add sample event data
+    sheet.appendRow([]);
+
+    final List<int>? bytes = excel.encode();
+
+    final Directory? directory = await getExternalStorageDirectory();
+    final String path = '${directory?.path}/Attendance_Report.xlsx';
+    final File file = File(path);
+
+    // Write the bytes to the file
+    await file.writeAsBytes(bytes!);
+
+    // Show a message to the user
+    final scaffoldContext = ScaffoldMessenger.of(context);
+    scaffoldContext.showSnackBar(
+      SnackBar(
+        content: Text('File downloaded to $path'),
+      ),
     );
   }
 }
